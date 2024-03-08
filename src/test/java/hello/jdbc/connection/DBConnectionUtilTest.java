e06e437c31585dfeaa94dd4073f19eb0c844477d
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-08 14:36:37 +0900

    commit(DB-part1): H2 db 연동

---
+++ src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,16 @@
+package hello.jdbc.connection;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class DBConnectionUtilTest {
+
+    @Test
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        assertThat(connection).isNotNull();
+    }
+}
