4949faae73f17e6b1c68661ac7b004b55cab39d8
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-08 16:51:12 +0900

    commit(DB-part1): 생성, 조회, 업데이트, 삭제 구현

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,17 +1,39 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
     @Test
     void save() throws SQLException {
+        //save
         Member member = new Member("memberV1", 10000);
         repositoryV0.save(member);
+
+        // findById
+        Member findMember = repositoryV0.findById(member.getMemberId());
+        log.info("who = {}", findMember);
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        //update: money: 10000 -> 20000
+        repositoryV0.update(member.getMemberId(), 20000);
+        Member updatedMember = repositoryV0.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repositoryV0.delete(member.getMemberId());
+        assertThatThrownBy(() -> repositoryV0.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
